92dbfb0d0d47506983588939422c5e6afcc69390
Omar Flores <cerberusprogrammer@gmail.com>
2024-06-15 14:32:03 -0700

    feat: Add HomeScreen widget for displaying Git repository information

---
+++ lib/home.screen.dart
@@ -0,0 +1,49 @@
+import 'package:clean_git/git.riverpod.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final gitRepoAsyncValue = ref.watch(gitRepoProvider);
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('Abrir explorador de archivos'),
+              onPressed: () async {
+                ref.read(gitRepoProvider.notifier).openDirectory();
+              },
+            ),
+            gitRepoAsyncValue.when(
+              data: (gitRepo) {
+                if (gitRepo.author.isEmpty &&
+                    gitRepo.commits.isEmpty &&
+                    gitRepo.branches.isEmpty) {
+                  return const Text('No data');
+                } else {
+                  return Column(
+                    children: [
+                      Text('Author: ${gitRepo.author}'),
+                      ...gitRepo.commits
+                          .map((commit) => Text('Commit: ${commit.sha}')),
+                      ...gitRepo.branches
+                          .map((branch) => Text('Branch: ${branch.name}')),
+                    ],
+                  );
+                }
+              },
+              loading: () => const CircularProgressIndicator(),
+              error: (error, stackTrace) => Text('Error: $error'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

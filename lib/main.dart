7ac399e179d7e2305645233b6e67ff6619eeee46
Omar Flores <cerberusprogrammer@gmail.com>
2024-06-14 22:10:59 -0700

    Update main.dart to add file picker functionality and integrate with Git

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,140 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:file_picker/file_picker.dart';
+import 'dart:io';
+import 'package:git/git.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: Locale('en', 'US'), // Define el locale
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
       theme: ThemeData(brightness: Brightness.dark),
-      home: Scaffold(
-        body: Center(
-          child: ElevatedButton(
-            child: Text('Abrir explorador de archivos'),
-            onPressed: () async {
-              try {
-                String? directoryPath =
-                    await FilePicker.platform.getDirectoryPath();
+      home: MyApp(),
+    );
+  }
+}
 
-                if (directoryPath != null) {
-                  print('Carpeta seleccionada: $directoryPath');
-                } else {
-                  // El usuario canceló la selección
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String authorName = '';
+  List<String> commits = [];
+  List<String> branches = [];
+
+  void showMessageDialog(BuildContext context, String title, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cerrar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text('Abrir explorador de archivos'),
+              onPressed: () async {
+                try {
+                  String? directoryPath =
+                      await FilePicker.platform.getDirectoryPath();
+
+                  if (directoryPath != null) {
+                    Directory directory = Directory(directoryPath);
+                    bool gitFolderExists =
+                        await Directory('${directory.path}/.git').exists();
+                    if (gitFolderExists) {
+                      GitDir gitDir = await GitDir.fromExisting(directory.path);
+                      if (gitDir != null) {
+                        showMessageDialog(context, 'Información',
+                            'El repositorio Git en $directoryPath es válido.');
+                        // Aquí puedes ejecutar comandos de Git, como obtener el nombre del autor
+                        ProcessResult result =
+                            await gitDir.runCommand(['config', 'user.name']);
+                        setState(() {
+                          authorName = result.stdout.trim();
+                        });
+                        // Obtener los commits
+                        ProcessResult resultCommits = await gitDir
+                            .runCommand(['log', '--pretty=format:%H %s']);
+                        setState(() {
+                          commits = resultCommits.stdout.trim().split('\n');
+                        });
+                        // Obtener las ramas
+                        ProcessResult resultBranches =
+                            await gitDir.runCommand(['branch', '-a']);
+                        setState(() {
+                          branches = resultBranches.stdout.trim().split('\n');
+                        });
+                      } else {
+                        showMessageDialog(context, 'Error',
+                            'El repositorio Git en $directoryPath no es válido.');
+                      }
+                    } else {
+                      showMessageDialog(context, 'Error',
+                          'La carpeta .git no fue encontrada en $directoryPath');
+                    }
+                  } else {
+                    // El usuario canceló la selección
+                  }
+                } catch (e) {
+                  showMessageDialog(context, 'Error',
+                      'No se encontró zenity. Por favor, instálalo usando tu gestor de paquetes.');
                 }
-              } catch (e) {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('Error'),
-                      content: Text(
-                          'No se encontró zenity. Por favor, instálalo usando tu gestor de paquetes.'),
-                      actions: <Widget>[
-                        TextButton(
-                          child: Text('Cerrar'),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                );
-              }
-            },
-          ),
+              },
+            ),
+            Text('Nombre del autor: $authorName'),
+            Text('Commits:'),
+            Expanded(
+              child: ListView.builder(
+                itemCount: commits.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(commits[index]),
+                  );
+                },
+              ),
+            ),
+            Text('Ramas:'),
+            Expanded(
+              child: ListView.builder(
+                itemCount: branches.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(branches[index]),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
